8ea7647379666cbda2bd0b1b3808620c21bb7776
shouyihua <yhshou@dlbdata.cn>
2017-12-29 14:46:27 +0800

    1111

--- module/lineGradient.js
+++ module/lineGradient.js
@@ -5,52 +5,54 @@ var option3={
         text: 'ECharts 入门示例'
     },
     tooltip: {},
-    legend: {
-        data:['销量'],
-        textStyle:{
-            fontSize: 16,
-            color: '#fff'
-        },
-        left:'78%'
-    },
+    grid:{
+        left:"1%",
+        right:"1%",
+        bottom:"1%"
+    } ,
     xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"],
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"],
         axisLabel: {
             textStyle: {
                 color: 'rgba(255, 255, 255,1)', //刻度颜色
-                fontSize: 14  //刻度大小
+                fontSize: 0  //刻度大小
             }
-        }
+        },
+        minInterval: 0,
+        show:false
+
     },
     yAxis: {
         axisLabel: {
             textStyle: {
                 color: 'rgba(255, 255, 255,1)', //刻度颜色
-                fontSize: 14  //刻度大小
+                fontSize: 0  //刻度大小
             }
-        }
+        },
+        show:false
     },
     series: [{
         name: '销量',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20],
+        data: [85, 80, 36, 80, 10, 20,85, 120, 136, 10, 10, 20,115, 20, 36, 10, 10, 20,5, 20, 36, 10, 10, 20,5, 20, 36, 10, 10, 20,5, 20, 36, 10, 10, 20],
         itemStyle: {
             normal: {
-                barBorderRadius: 5,
                 color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
                     offset: 0,
-                    color: '#99d9ea'
-                }, {
+                    color: '#4cfafd'
+                },{
+                    offset: 0.5,
+                    color: '#46a4f7'
+                },{
                     offset: 1,
-                    color: '#3fa7dc'
+                    color: '#436df4'
                 }]),
                 shadowColor: 'rgba(0, 0, 0, 0.4)',
                 shadowBlur: 20
             }
         }
-    }],
-
-
+    }]
+    /*,
     animationEasing: 'elasticOut',
     animationEasingUpdate: 'elasticOut',
     animationDelay: function (idx) {
@@ -58,6 +60,6 @@ var option3={
     },
     animationDelayUpdate: function (idx) {
         return idx * 20;
-    }
+    }*/
 };
 myCharts3.setOption(option3)
