2776c9bbf63d2b1c5322b48b6e2fe5961f450ba4
shouyihua <yhshou@dlbdata.cn>
2017-12-29 10:07:35 +0800

    update

--- device.js
+++ device.js
@@ -1,5 +1,5 @@
 (function(){function getClientWidth(){
-    var cw=document.documentElement.clientWidth;/*cw=cw>750?750:cw*/;
+    var cw=document.documentElement.clientWidth;;
     document.documentElement.style.setProperty("font-size",cw/38.4+"px")}
     var Timer=null;window.addEventListener("resize",function(){clearTimeout(Timer),Timer=setTimeout(getClientWidth,300)},false);getClientWidth()})(window);
 
